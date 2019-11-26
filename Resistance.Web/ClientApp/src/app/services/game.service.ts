de1640918135b335bd78711f1b3511f2d3a2bcb5
Bradley Hopkinson <bradley.hopkinson@live.com>
2019-11-26 19:00:31 +0000

    Remove signalr node package

--- Resistance.Web/ClientApp/src/app/services/game.service.ts
+++ Resistance.Web/ClientApp/src/app/services/game.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 import { Subject, ReplaySubject } from 'rxjs';
-import * as signalR from '@microsoft/signalr';
 import { Router } from '@angular/router';
 import { PlayerDetails } from '../models/PlayerDetails';
 import { GamePlayer } from '../models/GamePlayer';
@@ -16,7 +14,6 @@ import { Role } from '../models/Role';
 export class GameService {
     private _messageReceived = new Subject();
     private _connectionEstablished = new Subject<Boolean>(); 
-    private _hubConnection: HubConnection;
     private _countdown = new Subject<boolean>();
     private _players = new ReplaySubject<PlayerDetails[]>();
     private _gameBoard = new Subject<GameBoard>();
@@ -35,80 +32,80 @@ export class GameService {
     public showLeaderScript = this._showLeaderScript.asObservable();
 
     constructor() {
-        this.createConnection();
-        this.registerOnClientEvents();
-        this.registerOnServerEvents();
-        this.startConnection();
+        // this.createConnection();
+        // this.registerOnClientEvents();
+        // this.registerOnServerEvents();
+        // this.startConnection();
     }
 
-    public JoinGame(initials: string) {
-        this.initials = initials;
-        let player: GamePlayer = { gameId: '0', playerInitials: initials };
-        this._hubConnection.invoke('JoinGame', player)
-        .catch(err => console.error(err));
-    }
+    // public JoinGame(initials: string) {
+    //     this.initials = initials;
+    //     let player: GamePlayer = { gameId: '0', playerInitials: initials };
+    //     this._hubConnection.invoke('JoinGame', player)
+    //     .catch(err => console.error(err));
+    // }
 
-    public PlayerReady(ready: boolean) {
-        this._hubConnection.invoke('PlayerReady', ready)
-        .catch(err => console.error(err));
-    }
+    // public PlayerReady(ready: boolean) {
+    //     this._hubConnection.invoke('PlayerReady', ready)
+    //     .catch(err => console.error(err));
+    // }
 
-    public StartGame() {
-        this._hubConnection.invoke('StartGame')
-            .catch(err => console.error(err));
-    }
+    // public StartGame() {
+    //     this._hubConnection.invoke('StartGame')
+    //         .catch(err => console.error(err));
+    // }
 
-    private createConnection() {
-        this._hubConnection = new HubConnectionBuilder()
-            .withUrl(window.location.href + 'game')
-            .configureLogging(signalR.LogLevel.Information)
-        .build();
-    }
+    // private createConnection() {
+    //     this._hubConnection = new HubConnectionBuilder()
+    //         .withUrl(window.location.href + 'game')
+    //         .configureLogging(signalR.LogLevel.Information)
+    //     .build();
+    // }
 
-    private startConnection(): void {
-        this._hubConnection
-        .start()
-        .then(() => {
-            this._connectionEstablished.next(true);
-        })
-        .catch(err => {
-            setTimeout(function () { this.startConnection(); }, 1000);
-        }); 
-    }
+    // private startConnection(): void {
+    //     this._hubConnection
+    //     .start()
+    //     .then(() => {
+    //         this._connectionEstablished.next(true);
+    //     })
+    //     .catch(err => {
+    //         setTimeout(function () { this.startConnection(); }, 1000);
+    //     }); 
+    // }
 
-    private registerOnClientEvents(): void {
-        this._hubConnection.onreconnected((connectionId: string, ) => {
-            this._connectionEstablished.next(true);
-            this._hubConnection.invoke('Reconnect');
-        });
+    // private registerOnClientEvents(): void {
+    //     this._hubConnection.onreconnected((connectionId: string, ) => {
+    //         this._connectionEstablished.next(true);
+    //         this._hubConnection.invoke('Reconnect');
+    //     });
 
-        this._hubConnection.onreconnecting(() => {
-            this._connectionEstablished.next(false);
-        });
+    //     this._hubConnection.onreconnecting(() => {
+    //         this._connectionEstablished.next(false);
+    //     });
 
-        this._hubConnection.onclose(() => {
-            this._connectionEstablished.next(false);
-        });
-    }
+    //     this._hubConnection.onclose(() => {
+    //         this._connectionEstablished.next(false);
+    //     });
+    // }
 
-    private registerOnServerEvents(): void {
-        this._hubConnection.on('UpdatePlayersList', (players: PlayerDetails[]) => {
-            this._players.next(players);
-        });
-        this._hubConnection.on('Countdown', (started: boolean) => {
-            this._countdown.next(started);
-        });
-        this._hubConnection.on('GameBoardChange', (gameBoard: GameBoard) => {
-            this._gameBoard.next(gameBoard);
-            this.leader = gameBoard.Leader;
-        });
-        this._hubConnection.on('ShowCharacter', (character: Character) => {
-            this.character = character;
-            this._characterAssigned.next(true);
+    // private registerOnServerEvents(): void {
+    //     this._hubConnection.on('UpdatePlayersList', (players: PlayerDetails[]) => {
+    //         this._players.next(players);
+    //     });
+    //     this._hubConnection.on('Countdown', (started: boolean) => {
+    //         this._countdown.next(started);
+    //     });
+    //     this._hubConnection.on('GameBoardChange', (gameBoard: GameBoard) => {
+    //         this._gameBoard.next(gameBoard);
+    //         this.leader = gameBoard.Leader;
+    //     });
+    //     this._hubConnection.on('ShowCharacter', (character: Character) => {
+    //         this.character = character;
+    //         this._characterAssigned.next(true);
             
-        });
-        this._hubConnection.on('ShowLeaderScript', (showScript: boolean) => {
-            this._showLeaderScript.next(showScript);
-        });
-    }
+    //     });
+    //     this._hubConnection.on('ShowLeaderScript', (showScript: boolean) => {
+    //         this._showLeaderScript.next(showScript);
+    //     });
+    // }
 }
